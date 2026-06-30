ec6d3586f9b02453cd15d250075ab3d093830464
Joshua Shin <j.sunkyu.shin@gmail.com>
2026-06-29 17:22:20 -0700

    adjust about me section

--- sonmatbyjosh-app/src/app/about/page.tsx
+++ sonmatbyjosh-app/src/app/about/page.tsx
@@ -26,15 +26,15 @@ export default function AboutPage() {
                 </h2>
 
                 <p className="font-about">
-                  Welcome to sonmat by josh! I created this site to document and
+                  Welcome to sonmat by josh! I created this site to 
                   highlight all the different foods I cook and eat. I'm a
-                  self-taught cook learning from watching youtube, tiktoks, ig
-                  reels, and watching my 엄마 (mom) cook from afar LOL. I'm going
+                  self-taught cook learning from watching youtube, ig
+                  reels, and watching my mom cook. I'm going
                   to be as transparent as I can on this site, showing the
-                  highlights but also lowlights in my culinary journey! Thanks for
+                  highlights but also the many fails that happen as I continue to cook and make content! Thanks for
                   checking out the site and if you ever decide to try making any
-                  of the recipes here, I would be super appreciative if you
-                  tagged me{" "}
+                  of the recipes here, 
+                  tag me{" "}
                     <a
                       href="https://www.instagram.com/sonmatbyjosh/"
                       target="_blank"
